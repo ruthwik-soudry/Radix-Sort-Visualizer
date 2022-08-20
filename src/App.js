33c8cdfc0f71671c11c61a945192bda707a1f082
Ruthwik Ravi Kumar Soudry <soudry.r@northeastern.edu>
2022-08-20 02:55:44 -0400

    minor fixes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 function App() {
   return (
     <div className="App">
+      <h1 className='main-heading'>Radix Sort Visualizer</h1>
       <SortingVisualizer></SortingVisualizer>
     </div>
   );
